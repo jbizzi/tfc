9d7672aad5ca46d2aeab6953634bd785be9afab0
j.bizzi <jbizzivelho@gmail.com>
2024-05-23 20:25:51 -0300

    Added ML approach

--- src/NeuralNetworkCorrection.py
+++ src/NeuralNetworkCorrection.py
@@ -2,48 +2,73 @@ import random
 
 import numpy as np
 import tensorflow as tf
-from src import Utils
+from src import Utils, HammingCode
 
 CHUNK_SIZE = 7
 
-
 def getDefaultData():
     return np.array((
-        [False, False, False, False],
-        [False, False, False, True],
-        [False, False, True, False],
-        [False, False, True, True],
-        [False, True, False, False],
-        [False, True, False, True],
-        [False, True, True, False],
-        [False, True, True, True],
-        [True, False, False, False],
-        [True, False, False, True],
-        [True, False, True, False],
-        [True, False, True, True],
-        [True, True, False, False],
-        [True, True, False, True],
-        [True, True, True, False],
-        [True, True, True, True],
+        [0, 0, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 1, 0],
+        [0, 0, 1, 1],
+        [0, 1, 0, 0],
+        [0, 1, 0, 1],
+        [0, 1, 1, 0],
+        [0, 1, 1, 1],
+        [1, 0, 0, 0],
+        [1, 0, 0, 1],
+        [1, 0, 1, 0],
+        [1, 0, 1, 1],
+        [1, 1, 0, 0],
+        [1, 1, 0, 1],
+        [1, 1, 1, 0],
+        [1, 1, 1, 1],
     ))
 
+def decode_sample(data):
+    decoded_data = []
+    for i in range(0, len(data) - 6, 7):
+        decoded_data.extend(HammingCode.decode(data[i:i + 7]).T)
+    return decoded_data
 
-def generate_data_for_training(noise_rates, total_samples):
+def encode_sample(data):
+    encoded_data = []
+    for i in range(0, len(data) - 3, 4):
+        encoded_data.extend(HammingCode.encode(data[i:i + 4]))
+    return encoded_data
 
+def generate_data_for_training(variancia, sample_length, Eb_db):
 
-    data = getDefaultData()
-    if (total_samples < len(data)): # not enough samples
+    amostra = np.random.choice([0, 1], size=int(sample_length))
+    amostra_codificada = encode_sample(amostra)
 
-    encoded_data = [Utils.encode_sample(bit) for bit in data]
+    for i in range(len(amostra_codificada)):
+        if amostra_codificada[i] == 0.0:
+            amostra_codificada[i] = -1.0
+        else:
+            amostra_codificada[i] = 1.0
 
+    Eb = 10**(Eb_db /10)
 
+    ruido = np.random.normal(0, np.sqrt(variancia/2), size=int(len(amostra_codificada)))
 
-    noisy_data = [Utils.noiseString(error_rate, word) for word, error_rate in zip(encoded_data, noise_rates)]
-    return noisy_data, data
+    amostra_ruidosa = np.sqrt(Eb) * np.array(amostra_codificada) + ruido
+
+    amostra_recebida = np.sign(amostra_ruidosa)
+
+    for i in range(len(amostra_recebida)):
+        if amostra_recebida[i] == -1:
+            amostra_recebida[i] = 0
+        else:
+            amostra_recebida[i] = 1
+
+    return amostra_recebida, amostra
 
 
 # Função para criar e treinar a rede neural
 def train_neural_network(noisy_data, original_data, epoches, sample_length):
+
     model = tf.keras.Sequential([
         tf.keras.layers.Dense(7, activation='relu', input_shape=(7,)),
         tf.keras.layers.Dense(7, activation='relu'),
@@ -64,7 +89,8 @@ def train_neural_network(noisy_data, original_data, epoches, sample_length):
         for index in range(0, len(original_sample), 4):
             original_data_reshaped.append(np.array(Utils.toInt(list(original_sample)[index:index + 4]), dtype=int))
     original_data_reshaped = np.array(original_data_reshaped)
-
+    print(noisy_data_reshaped)
+    print(original_data_reshaped)
     model.fit(
         noisy_data_reshaped,
         original_data_reshaped,
@@ -73,26 +99,21 @@ def train_neural_network(noisy_data, original_data, epoches, sample_length):
     )
     return model
 
-
-def generate_data_for_testing(word_length, noise_rates, words):
-    data = []
-    for i in range(0, words):
-        data.append(np.random.choice([True, False], size=word_length))
-
-    encoded_data = [Utils.encode_sample(bit) for bit in data]
-
-    noisy_data = [Utils.noiseString(random.randint(0, 100), word) for word in encoded_data]
-    return noisy_data, data
-
-
 def decode_and_correct(encoded_data, model):
     decoded_data = []
     for input_index in range(0, len(encoded_data), 7):
         input = np.array(Utils.toInt(encoded_data[input_index:input_index + 7]))
         input = tf.expand_dims(input, axis=0)
         decoded_data.append(model.predict(input))
-    decoded_data = np.round(decoded_data)
-    return ''.join(str(int(each)) for each in np.concatenate(np.concatenate(decoded_data)))
+
+    decoded = []
+    for each in decoded_data:
+        for bit_array in each:
+            for bit in bit_array:
+                corrected_bit = 1 if bit >= 0.5 else 0
+                decoded.append(corrected_bit)
 
 
-generate_data_for_training(np.linspace(0, 1, 101))
+    return decoded #''.join((int(each)) for each in np.concatenate(np.concatenate(decoded_data)))
+
+
